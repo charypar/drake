cbc3d340b534414583ad8ca285610b10dc072440
Viktor Charypar <charypar@gmail.com>
2023-11-12 18:50:00 +0000

    Add basic CLI with Clap

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,53 @@
 use std::env;
 
+use clap::{Parser, Subcommand};
+
 use drake::Drake;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// Scan a path and index the declarations and references
+    Scan {
+        /// Path to scan
+        #[arg(default_value = ".")]
+        path: String,
+    },
+
+    /// Print contents of specific files
+    Print {
+        /// Path to scan
+        #[arg(default_value = ".")]
+        path: String,
+        #[arg(short, long = "decl")]
+        declarations: bool,
+        #[arg(short, long = "refs")]
+        references: bool,
+        #[arg(long)]
+        full: bool,
+    },
+}
+
 fn main() -> anyhow::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    let path = &args[1];
+    let cli = Cli::parse();
 
     let mut drake = Drake::new();
 
-    println!("Prinitng swift files in path {}", path);
-    drake.print(path)
+    match &cli.command {
+        Command::Scan { path } => drake.scan(&path)?,
+        Command::Print {
+            path,
+            declarations,
+            references,
+            full,
+        } => drake.print(&path)?,
+    }
+
+    Ok(())
 }
